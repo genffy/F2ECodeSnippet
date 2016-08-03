638de74d33257e8547571a1ac1457228bf7eba2e
genffy <lee.genffy@gmail.com>
2016-08-03 10:06:38 +0800

    add freezen columns

---
+++ components/freeze-column/src/index.jsx
@@ -0,0 +1,29 @@
+/**
+ * http://demos.telerik.com/kendo-ui/grid/frozen-columns
+ */
+import React, {Component, PropTypes} from 'react';
+
+export default class index extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            // TODO
+        }
+    }
+
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillReceiveProps() {
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
