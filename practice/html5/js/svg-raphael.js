b86ebb9e5f869780d8be24aeae92c7110868cf03
genffy <lee.genffy@gmail.com>
2016-11-14 08:23:40 +0800

    update

--- practice/html5/js/svg-raphael.js
+++ practice/html5/js/svg-raphael.js
@@ -1,40 +1,8 @@
 /**
  * Created by genffy on 2016/11/4.
  */
-console.log('test');
-/*
-var $canvas, HEIGHT, WIDTH, arrow_1, center, paper, radius, stroke;
-$canvas = document.getElementById('arrows');
-paper = Raphael($canvas, 500, 500);
-WIDTH = 500;
-HEIGHT = 500;
-center = [WIDTH / 2, HEIGHT / 2];
-radius = 230;
-stroke = 5;
-
-paper.customAttributes.arc = function(center_x, center_y, degrees, radius) {
-    var arc_x, arc_y, path, radians;
-    if (!radius) {
-        return;
-    }
-    radians = (90 - degrees) * Math.PI / 180;
-    arc_x = center_x + radius * Math.cos(radians);
-    arc_y = center_y - radius * Math.sin(radians);
-    path = [['M', center_x, center_y - radius], ['M', center_x, center_y - radius], ['A', radius, radius, 0, +(degrees > 180), 1, arc_x, arc_y]];
-    return {
-        path: path
-    };
-};
-arrow_1 = paper.path();
-arrow_1.node.setAttribute('class', 'arrow_1');
-arrow_1.attr({
-    arc: [center[0], center[1], 10, radius]
-});
-arrow_1.attr({ 'arrow-end': 'classic-wide-long', 'arrow-start': 'classic-wide-long' });
-arrow_1.rotate(80, center[1], center[0]);
-*/
-
 Raphael.fn.connection = function (obj1, obj2, line, bg, arrow) {
+    bg = 'transparent';
     if (obj1.line && obj1.from && obj1.to) {
         line = obj1;
         obj1 = line.from;
@@ -77,11 +45,14 @@ Raphael.fn.connection = function (obj1, obj2, line, bg, arrow) {
         x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
         y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
     var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+
     if (line && line.line) {
         line.bg && line.bg.attr({path: path});
         line.line.attr({path: path});
+        line.line.attr({ 'arrow-end': 'classic-wide-long'});
     } else {
         var color = typeof line == "string" ? line : "#000";
+        this.path(path).attr({'arrow-end': 'classic-wide-long'});
         return {
             bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
             line: this.path(path).attr({stroke: color, fill: "none"}),
@@ -91,6 +62,12 @@ Raphael.fn.connection = function (obj1, obj2, line, bg, arrow) {
     }
 };
 
+function objLoad(){
+    $(document.getElementById('flow').contentDocument).find('#rect_zsjyj').click(function(evt) {
+        console.log('你点到我了', evt)
+    })
+}
+
 $(function() {
     var paper = Raphael("arrows", 940, 738),
         connections = [],
@@ -99,36 +76,38 @@ $(function() {
             paper.rect(0, 10, 80, 40, 2),
             paper.rect(120, 10, 80, 40, 2),
             paper.rect(240, 10, 80, 40, 2), //1111
-
             paper.rect(360, 10, 80, 40, 2),
             // 竖
             paper.rect(120, 70, 80, 40, 2),// 1111
-
             // 空点
             paper.rect(199.5, 149, 1, 1, 0),
-
             paper.rect(160, 200, 80, 40, 2),
             // 空点
-            paper.rect(199.5, 250, 1, 1, 2),
-
+            paper.rect(199.5, 250, 1, 1, 0),
             paper.rect(160, 260, 80, 40, 2),
             paper.rect(160, 320, 80, 40, 2),
             paper.rect(160, 380, 80, 40, 2),
             paper.rect(160, 440, 80, 40, 2),
             paper.rect(160, 520, 80, 40, 2),
-            // paper.rect(160, 600, 80, 40, 2),
             // 其他
             paper.rect(20, 200, 80, 40, 2),
-            paper.rect(280, 230, 80, 40, 2),
+            paper.rect(280, 230, 80, 40, 2)
         ];
     for (var i = 0, ii = shapes.length; i < ii; i++) {
-        var color = Raphael.getColor();
-        console.log(color);
-        shapes[i].attr({fill: '#fff', stroke: '#000', "fill-opacity": 0, "stroke-width": 2, cursor: "pointer"});
+        var color = Raphael.getColor(),
+            _paper = shapes[i];
+        _paper.attr({
+            'text':'1234',
+            'fill': '#E7CBBA',
+            'stroke': '#5A9A93',
+            'stroke-width': 2,
+            'cursor': 'pointer'
+        });
+        //paper.text(40, 15, '线圈绝缘体');
         // shapes[i].drag(move, dragger, up);
     }
 
-    paper.path("M160 110L160 149L280 149L280 50");
+    paper.path("M160 110L160 149L280 149L280 50").attr({stroke: '#000', fill: "none"});
     // 链接
     connections.push(paper.connection(shapes[0], shapes[1], "#000", "#fff"));
     connections.push(paper.connection(shapes[3], shapes[2], "#000", "#fff"));
@@ -147,6 +126,4 @@ $(function() {
 
     connections.push(paper.connection(shapes[13], shapes[6], "#000", "#fff"));
     connections.push(paper.connection(shapes[14], shapes[7], "#000", "#fff"));
-
-
 });
