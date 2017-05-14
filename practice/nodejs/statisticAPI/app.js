450c8b61efa89bf9ace411f6d628166acea0413c
genffy <lee.genffy@gmail.com>
2017-05-14 21:32:41 +0800

    update

--- practice/nodejs/statisticAPI/app.js
+++ practice/nodejs/statisticAPI/app.js
@@ -1,12 +1,17 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const opn = require('opn')
 
+const PORT = 3001
 app.set('view engine', 'pug')
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
-app.listen(3001, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`)
+  opn(`http://127.0.0.1:${PORT}`, {app: ['google chrome', '--incognito']}).then(() => {
+	  
+  })
 })
