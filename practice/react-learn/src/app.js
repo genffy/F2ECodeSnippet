e62baa71d14936be763fa33c8eb001762919babd
Genffy <lee.genffy@gmail.com>
2017-05-17 07:39:56 +0800

    add redux

--- practice/react-learn/src/app.js
+++ practice/react-learn/src/app.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
 
 import AppRouter from './router'
 
+import reducer from './reducer'
+
 import '../styles/index.scss'
+
+let store = createStore(reducer)
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component/>
+      <Provider store={store}>
+        <Component/>
+      </Provider>
     </AppContainer>,
     document.getElementById('app')
   )
--- practice/react-learn/src/router.js
+++ practice/react-learn/src/router.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {Router, Route, Redirect} from 'react-router-dom'
 import createHashHistory from 'history/createHashHistory'
-
+import {connect} from 'react-redux'
 import Index from './index'
 import Detail from './detail'
 
-export default class AppRouter extends Component {
+class AppRouter extends Component {
     constructor(props) {
         super(props)
         this.history = new createHashHistory()
@@ -22,3 +22,37 @@ export default class AppRouter extends Component {
         )
     }
 }
+
+AppRouter.propTypes = {
+  visibleTodos: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired
+  }).isRequired).isRequired,
+  visibilityFilter: PropTypes.oneOf([
+    'SHOW_ALL',
+    'SHOW_COMPLETED',
+    'SHOW_ACTIVE'
+  ]).isRequired
+}
+
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed)
+  }
+}
+
+// Which props do we want to inject, given the global state?
+// Note: use https://github.com/faassen/reselect for better performance.
+function select(state) {
+  return {
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    visibilityFilter: state.visibilityFilter
+  }
+}
+
+export default connect(select)(AppRouter)
